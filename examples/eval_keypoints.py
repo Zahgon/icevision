d47e718172e62a107a76192929f6afca35c51b87
potipot <pawel.potrykus@hayden.ai>
2024-11-22 15:25:05 +0100

    update eval script

--- examples/eval_keypoints.py
+++ examples/eval_keypoints.py
@@ -15,30 +15,31 @@ from icevision import tfms
 
 
 def main():
-    data_dir = Path.home() / "datasets/plate_localization/v2.1"
+    data_dir = Path.home() / "datasets/plate_localization/v2.2"
     parser = VLPParser(annotations_filepath=data_dir / "metadata.csv")
 
     train_records, valid_records = parser.parse(data_splitter=FolderSplitter(["train", "val"]), cache_filepath=data_dir/"cache_manual_visibility")
 
     # Create the parser
     image_size = 384
-    ckpt_path = "/home/ppotrykus/Programs/icevision/icevision-2.0-keypoints/t74d6s3g/checkpoints/056000_loss=0.00_PCK@0.1=0.932.ckpt"
+    noisy_ckpt = "/home/ppotrykus/Programs/icevision/icevision-2.0-keypoints/92yzbaf1/checkpoints/042350_loss=0.00_PCK@0.1=0.847.ckpt"
+    subset_ckpt = "/home/ppotrykus/Programs/icevision/icevision-2.0-keypoints/9nxwzqxt/checkpoints/008400_loss=0.00_PCK@0.1=0.723.ckpt"
     torch_compile = False
-    ignore_invisible = False
+    ignore_invisible = True
     valid_tfms = tfms.A.Adapter([*tfms.A.resize_and_pad(image_size), tfms.A.Normalize()])
 
     # Datasets
     valid_ds = Dataset(valid_records, valid_tfms)
 
     model_type = models.custom.keypoints
-    backbone = model_type.backbones.tf_efficientnet_b2
+    backbone = model_type.backbones.tf_efficientnet_b0
     model = model_type.model(backbone=backbone(pretrained=False), num_keypoints=1, use_visibility=True)
 
     # Data Loaders
-    num_workers = 6
-    valid_dl = model_type.valid_dl(valid_ds, batch_size=32, num_workers=num_workers, shuffle=False)
+    num_workers = 0
+    valid_dl = model_type.valid_dl(valid_ds, batch_size=16, num_workers=num_workers, shuffle=False)
 
-    light_model = model_type.lightning.ModelAdapter.load_from_checkpoint(ckpt_path, model=model, torch_compile=torch_compile, ignore_invisible=ignore_invisible)
+    light_model = model_type.lightning.ModelAdapter.load_from_checkpoint(noisy_ckpt, model=model, torch_compile=torch_compile, ignore_invisible=ignore_invisible)
     trainer = L.Trainer(accelerator='gpu', precision="16-mixed", enable_checkpointing=False, limit_test_batches=1.0)
     # trainer.fit(light_model, valid_dl)
     trainer.test(light_model, valid_dl)
